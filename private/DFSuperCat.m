5a7444e2555973a2d66d5772e967756128045ee9
vhtmf <vhtmf@EbolaChan>
2021-03-09 18:01:36 +0800

    DimensionFun支持同构数组拼接

---
+++ private/DFSuperCat.m
@@ -0,0 +1,18 @@
+function varargout = DFSuperCat(varargout,CatMode,SplitDimensions)
+switch CatMode
+	case "EsNlcs"
+		varargout=cellfun(@cell2mat,varargout,"UniformOutput",false);
+	case "Linear"
+		varargout=cellfun(@(Out)cat(SplitDimensions,Out{:}),varargout,"UniformOutput",false);
+	case "HomoArray"
+		NoSD=numel(SplitDimensions);
+		for a=1:nargout
+			Out=varargout{a};
+			for b=1:NoSD-1
+				SD=SplitDimensions(b);
+				Out=cellfun(@(O)cat(SD,O{:}),num2cell(Out,SplitDimensions(b)),"UniformOutput",false);
+			end
+			varargout{a}=cat(SplitDimensions(end),Out{:});
+		end
+end
+end
