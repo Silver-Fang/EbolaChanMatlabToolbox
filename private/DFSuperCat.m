38cb3bd290e0f4785308242f134a8e3dcb9e1564
埃博拉酱 <Leenrung@outlook.com>
2021-03-26 12:13:23 +0800

    修复DimensionFun一个罕见bug

--- private/DFSuperCat.m
+++ private/DFSuperCat.m
@@ -3,7 +3,12 @@ switch CatMode
 	case "EsNlcs"
 		varargout=cellfun(@cell2mat,varargout,"UniformOutput",false);
 	case "Linear"
-		varargout=cellfun(@(Out)cat(SplitDimensions,Out{:}),varargout,"UniformOutput",false);
+		if isempty(SplitDimensions)
+			%这里不能使用varargout=horzcat(varargout{:})，因为varargout元胞里有可能是空的
+			varargout=cellfun(@(Out)vertcat(Out{:}),varargout,"UniformOutput",false);
+		else
+			varargout=cellfun(@(Out)cat(SplitDimensions,Out{:}),varargout,"UniformOutput",false);
+		end
 	case "CanCat"
 		varargout=cellfun(@SuperCell2Mat,varargout,"UniformOutput",false);
 end
